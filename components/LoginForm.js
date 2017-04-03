65e2cdc904d286a64d4fb4e697fe7bcda17698e5
hwalborn <hwalborn@gmail.com>
2017-04-03 16:59:00 -0400

    Ian, Corinne, and Niky all helped me

--- components/LoginForm.js
+++ components/LoginForm.js
@@ -4,26 +4,51 @@ class LoginForm extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      username: '',
+      password: '',
+    };
+    this.saveUsername = this.saveUsername.bind(this)
+    this.savePassword = this.savePassword.bind(this)
+    this.preSubmit = this.preSubmit.bind(this)
+  }
+
+  saveUsername(event){
+    this.setState({
+      username: event.target.value
+    })
+  }
+
+  savePassword(event){
+    this.setState({
+      password: event.target.value
+    })
+  }
+
+  preSubmit(event){
+    event.preventDefault()
+    if((this.state.username.length) !== 0 && (this.state.password.length !== 0)){
+      this.props.onSubmit()
+    }
   }
 
   render() {
     return (
-      <form>
+      <form onSubmit={this.preSubmit}>
         <div>
           <label>
             Username
-            <input id="test-username" type="text" />
+            <input id="test-username" type="text" onChange={this.saveUsername} />
           </label>
         </div>
         <div>
           <label>
             Password
-            <input id="test-password" type="password" />
+            <input id="test-password" type="password" onChange={this.savePassword} />
           </label>
         </div>
         <div>
-          <button type="submit">Log in</button>
+          <button type="submit" >Log in</button>
         </div>
       </form>
     );
--- components/PoemWriter.js
+++ components/PoemWriter.js
@@ -4,14 +4,45 @@ class PoemWriter extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      value: '',
+      isValid: false
+    };
+    this.updateText = this.updateText.bind(this)
+    this.validatePoem = this.validatePoem.bind(this)
+  }
+
+  updateText(event){
+    this.setState({
+      value: event.target.value,
+      isValid: this.validatePoem(event.target.value),
+    })
+
+  }
+
+  validatePoem(poem) {
+    var line1, line2, line3
+    let lines = poem.split('\n')
+    if(lines.length === 3){
+      line1 = lines[0].trim().split(' ').length
+      line2 = lines[1].trim().split(' ').length
+      line3 = lines[2].trim().split(' ').length
+    }
+    if((lines.length === 3) && (line1 === 5) && (line2 === 3) && (line3 === 5)){
+      return true
+    }else{
+      return false
+    }
   }
 
   render() {
+    let niky = <div id="poem-validation-error" style={{color: 'red'}}>This poem is not written in the right structure!</div>
     return (
       <div>
-        <textarea rows="3" cols="60" />
-        <div id="poem-validation-error" style={{color: 'red'}}>This poem is not written in the right structure!</div>
+        <textarea rows="3" cols="60" onChange={this.updateText} />
+        {
+          this.state.isValid ? null : niky
+        }
       </div>
     );
   }
