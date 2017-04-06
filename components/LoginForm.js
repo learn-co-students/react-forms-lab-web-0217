15f9d27ea4a2b7e4f67da40ab7ec6a0f1ec64ec3
dgwp-projects <zayneabraham1@gmail.com>
2017-04-06 13:52:22 -0400

    Done.

--- components/LoginForm.js
+++ components/LoginForm.js
@@ -4,22 +4,49 @@ export default class LoginForm extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      username: '',
+      password: '',
+    };
+  
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(field,event){
+
+    this.setState({
+      [field]: event.target.value
+    })
+  }
+  
+  handleSubmit(e){
+    e.preventDefault();
+    const{username,password} = this.state
+    
+    if (!username || !password) {
+      return;
+    }
+
+    this.props.onSubmit({
+      username,
+      password
+    })
   }
 
   render() {
     return (
-      <form>
+      <form onSubmit={this.handleSubmit}>
         <div>
           <label>
             Username
-            <input id="test-username" type="text" />
+            <input id="test-username" type="text" value={this.state.username} onChange={this.handleChange.bind(this, 'username')} />
           </label>
         </div>
         <div>
           <label>
             Password
-            <input id="test-password" type="password" />
+            <input id="test-password" type="password" value={this.state.password} onChange={this.handleChange.bind(this, 'password')} />
           </label>
         </div>
         <div>
@@ -29,3 +56,7 @@ export default class LoginForm extends React.Component {
     );
   }
 }
+
+LoginForm.propTypes = {
+  onSubmit: React.PropTypes.func,
+};
