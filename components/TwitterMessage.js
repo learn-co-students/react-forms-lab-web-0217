1294a981c0cb64f9162030b921fa9c646452bb81
“aroncey” <artuhrroncey@gmail.comssh-keygen -t rsa -C arthurroncey@gmail.com>
2017-04-06 15:34:36 -0400

    Done.

--- components/TwitterMessage.js
+++ components/TwitterMessage.js
@@ -4,14 +4,26 @@ export default class TwitterMessage extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      value: '',
+    }
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event){
+    // console.log(event.target.value)
+    this.setState({
+      value: event.target.value,
+    });
   }
 
   render() {
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" />
+        <p>{this.props.maxChars - this.state.value.length}</p>
+        <input type="text" value={this.state.value} onChange={this.handleChange} />
       </div>
     );
   }
