51747a191fc711f52336723add892c0068730bfc
chasms <chasms@chas.ms>
2017-04-06 15:24:05 -0400

    comet

--- components/PoemWriter.js
+++ components/PoemWriter.js
@@ -4,21 +4,28 @@ export default class PoemWriter extends React.Component {
   constructor() {
     super();
 
-    this.state = {poem: ""};
+    this.state = {poem: "", valid: false};
     this.handleChange = this.handleChange.bind(this)
+
   }
 
   handleChange(e) {
     this.setState({
-      [e.target.name]: e.target.value
+      [e.target.name]: e.target.value,
+      valid: this.isValid(e.target.value)
     })
+  }
+
+  isValid(poem) {
+    let lines = poem.split(`\n`).map( line => line.trim().split(' ') )
+    return lines.length === 3 && lines[0].length === 5 && lines[1].length === 3 && lines[2].length === 5
   }
 
   render() {
     return (
       <div>
-        <textarea onChange="this.handleChange" name="poem" rows="3" cols="60" />
-        <div id="poem-validation-error" style={{color: 'red'}}>This poem is not written in the right structure!</div>
+        <textarea onChange={this.handleChange} name="poem" rows="3" cols="60" />
+        {this.state.valid ? <div id="valid">How very poetic!</div> : <div id="poem-validation-error" style={{color: 'red'}}>This poem is not written in the right structure!</div>}
       </div>
     );
   }
--- components/TwitterMessage.js
+++ components/TwitterMessage.js
@@ -4,14 +4,22 @@ export default class TwitterMessage extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {tweet: ''};
+    this.handleChange = this.handleChange.bind(this)
+  }
+
+  handleChange(e) {
+    this.setState({
+      tweet: e.target.value
+    })
   }
 
   render() {
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" />
+        <input onChange={this.handleChange} type="text" />
+        <p>Characters remaining: {this.props.maxChars - this.state.tweet.length}</p>
       </div>
     );
   }
